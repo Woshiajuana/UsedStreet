d3a4b432879997fcb95006615535368222ada3be
Woshiajuana <zhigang.chen@owulia.com>
2017-05-24 16:35:07 +0800

    搭建环境，安装依赖初步完善

---
+++ UsedStreetWap/src/assets/lib/Tool.js
@@ -0,0 +1,94 @@
+/**
+ * Created by Administrator on 2017/5/2.
+ */
+const Tool = function (win) {
+    let Tool = {};
+    /**
+     * 操作数据到sessionstorage
+     * */
+    Tool.dataToSessionStorageOperate = {
+        /**存储*/
+        save: function (data_name,data_value) {
+            if(typeof data_name != 'undefined' && typeof data_value != 'undefined')
+                sessionStorage.setItem(data_name,JSON.stringify(data_value));
+        },
+        /**取出*/
+        achieve: function (data_name) {
+            var data_value = sessionStorage.getItem(data_name);
+            data_value && (data_value = JSON.parse(data_value));
+            return data_value;
+        },
+        /**删除*/
+        remove: function (data_name) {
+            if(data_name)
+                sessionStorage.removeItem(data_name);
+        },
+        /**清空*/
+        clear: function () {
+            sessionStorage.clear();
+        }
+    };
+    /**
+     * 操作数据到localstorage
+     * */
+    Tool.dataToLocalStorageOperate = {
+        /**存储*/
+        save: function (data_name,data_value) {
+            if(typeof data_name != 'undefined' && typeof data_value != 'undefined')
+                localStorage.setItem(data_name,JSON.stringify(data_value));
+        },
+        /**取出*/
+        achieve: function (data_name) {
+            var data_value = localStorage.getItem(data_name);
+            data_value && (data_value = JSON.parse(data_value));
+            return data_value;
+        },
+        /**删除*/
+        remove: function (data_name) {
+            if(data_name)
+                localStorage.removeItem(data_name);
+        },
+        /**清空*/
+        clear: function () {
+            localStorage.clear();
+        }
+    };
+
+    /**
+     * 跳转页面
+     * */
+    Tool.jumpPage = function ( jumpUrl ) {
+        if(typeof jumpUrl == 'undefined') win.location.href = win.location.origin + '/#/abnormal';
+        else if( jumpUrl.indexOf('http') == -1)
+            win.location.href = win.location.origin + '/#/' + jumpUrl;
+        else
+            win.location.href = jumpUrl;
+    };
+
+    /**
+     * 格式化时间
+     * */
+    Tool.format = function(fmt,date) {
+        var time = date || new Date();
+        var o = {
+            "M+" : time.getMonth()+1,
+            "d+" : time.getDate(),
+            "h+" : time.getHours(),
+            "m+" : time.getMinutes(),
+            "s+" : time.getSeconds(),
+            "q+" : Math.floor((time.getMonth()+3)/3),
+            "S"  : time.getMilliseconds()
+        };
+        if(/(y+)/.test(fmt)) {
+            fmt=fmt.replace(RegExp.$1, (time.getFullYear()+"").substr(4 - RegExp.$1.length));
+        }
+        for(var k in o) {
+            if(new RegExp("("+ k +")").test(fmt)){
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+            }
+        }
+        return fmt;
+    };
+    return Tool;
+} (window);
+export default Tool;
---
+++ UsedStreetWap/src/store/index.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Administrator on 2017/4/18.
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+import types from './mutation-types.js'
+Vue.use(Vuex);
+
+/**
+ * 数据存储
+ * */
+const state = {
+
+};
+
+/**
+ * 模块
+ * */
+const modules = {
+
+};
+
+/**
+ * 提交异步请求
+ * */
+const actions = {
+
+};
+
+/**
+ * 提交同步请求
+ * */
+const mutations = {
+
+
+};
+
+export default new Vuex.Store({
+    state,
+    modules,
+    actions,
+    mutations
+})
