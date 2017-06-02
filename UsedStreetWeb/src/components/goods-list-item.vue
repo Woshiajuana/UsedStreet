1d8c7d4e2db37510a3cedf87e2a9d6210f2773e4
Woshiajuana <zhigang.chen@owulia.com>
2017-06-02 14:21:27 +0800

    个人中心我的主页页面需求

---
+++ UsedStreetWeb/src/components/goods-list-item.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="goods-list-item">
+        <img src="../assets/img/1.jpg" alt="" class="goods-list-item-type-img">
+        <p class="goods-list-item-name">裙子</p>
+        <p class="goods-list-item-sum">50</p>
+        <p class="goods-list-item-time">2017 06 02</p>
+        <i class="goods-list-item-cancel">取消</i>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'goods-list-item'
+    }
+</script>
+<style lang="scss">
+    @import "../assets/scss/define";
+    .goods-list-item{
+        @extend %pr;
+        /*height: 120px;*/
+        padding: 10px 10px 10px 110px;
+        border-bottom: 1px solid #ccc;
+    }
+    .goods-list-item-type-img{
+        @extend %pa;
+        @extend %t50;
+        @extend %l0;
+        width: 100px;
+        height: 100px;
+        margin-top: -50px;
+    }
+    .goods-list-item-name{
+        @extend %f16;
+        height: 50px;
+        line-height: 50px;
+    }
+    .goods-list-item-sum{
+        @extend %pr;
+        @extend %tar;
+        font-size: 36px;
+        height: 50px;
+        line-height: 50px;
+        padding-right: 20px;
+        &:after{
+            @extend %pa;
+            @extend %r0;
+            @extend %f14;
+            content: '￥';
+        }
+    }
+    .goods-list-item-time{
+        @extend %oh;
+        @extend %tar;
+        @extend %f12;
+        @extend %c9;
+        margin-right: 40px;
+        height: 20px;
+        line-height: 20px;
+    }
+    .goods-list-item-cancel{
+        @extend %pa;
+        @extend %f12;
+        @extend %cfff;
+        @extend %cp;
+        padding: 0 5px;
+        right: 10px;
+        bottom: 12px;
+        background-color: #FF4949;
+        border-radius: 5px;
+    }
+</style>
--- UsedStreetWeb/src/views/user/children/Personal.vue
+++ UsedStreetWeb/src/views/user/children/Personal.vue
@@ -1,15 +1,40 @@
 <template>
     <div class="user-container personal-wrap">
         <h1 class="user-container-title">我的主页：</h1>
+        <div class="goods-list">
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+            <goods-list-item></goods-list-item>
+        </div>
     </div>
 </template>
 <script>
+    import GoodsListItem from '../../../components/goods-list-item.vue'
     export default {
-        name: 'personal'
+        name: 'personal',
+        components: {
+            GoodsListItem
+        }
     }
 </script>
 <style lang="scss">
     @import "../../../assets/scss/define";
+    .goods-list{
+        margin: 10px 20px;
+    }
     .user-container-title{
         @extend %f16;
         @extend %fwn;
